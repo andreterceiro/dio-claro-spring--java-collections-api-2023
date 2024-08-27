016d47296467889dfa13a8d2a0f4b95b2ce58a24
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-27 13:43:43 -0300

    Generating the constructor, the porperty 'descricao' and the method 'getDesricao()' as teached in the video

--- src/list/operacoesBasicas/Tarefa.java
+++ src/list/operacoesBasicas/Tarefa.java
@@ -1,7 +1,30 @@
 package list.operacoesBasicas;
 
 public class Tarefa {
+    /**
+     * Atributo que armazena a descrição da tarefa
+     */
+    String descricao;
+
     public static void main(String[] args) {
         System.out.println(("oi"));
     }
+
+    /**
+     * Construtor
+     * 
+     * @param descricao Descrição da tarefa
+     */
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * Getter de this.descricao
+     */
+    String getDescricao() {
+        return this.descricao;
+    }
+
+
 }
